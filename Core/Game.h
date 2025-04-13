21593bdbca1206f3a716bd3df718207df4dd60c8
Pedro Delgadillo <91334249+petedillo@users.noreply.github.com>
2025-04-13 18:34:30 -0500

    Add main logic to game class

--- Core/Game.h
+++ Core/Game.h
@@ -4,13 +4,25 @@
 
 #ifndef GAME_H
 #define GAME_H
-
-
+#include "../Entites/Player.h"
+#include <iostream>
 
 class Game {
+public:
+    static void gameStart() {
+        Player player;
 
+        std::cout << "Hello Player what is your name?\n";
+        std::cin >> player.name;
+
+
+        if (player.name == "Pedro") {
+            std::cout << "Nice Job! Me\n" << "Your Current Health is " << player.currentHealth << "\n";
+        } else {
+            std::cout << "Hello " << player.name << ", welcome to the game!\n" << std::endl;
+        }
+    }
 };
-
 
 
 #endif //GAME_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 
-#include "Entites/Player.h"
+#include "Core/Game.h"
 
 int main() {
-    Player player;
-
-    std::cout << "Hello Player what is your name?\n";
-    std::cin >> player.name;
-
-
-    if (player.name == "Pedro") {
-        std::cout << "Nice Job! Me\n" << "Your Current Health is " << player.currentHealth << "\n";
-    } else {
-        std::cout << "Hello " << player.name << ", welcome to the game!\n" << std::endl;
-    }
-
+    Game::gameStart();
     return 0;
 }
