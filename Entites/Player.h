a45d9e23843de9ba011bb8061232928044f40091
Pedro Delgadillo <91334249+petedillo@users.noreply.github.com>
2025-04-13 20:34:10 -0500

    create addItem, viewInventory, and useItem methods in player class

--- Entites/Player.h
+++ Entites/Player.h
@@ -1,8 +1,8 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <string>
-#include <algorithm>
+#include  <iostream>
+#include "Item.h"
 
 class Player {
 public:
@@ -10,6 +10,7 @@ public:
     int maxHealth = 100;
     int currentHealth = 100;
     int level = 1;
+    std::vector<Item> inventory;
 
     void takeDamage(int amount) {
         currentHealth = std::max(0, currentHealth - amount);
@@ -17,6 +18,42 @@ public:
 
     void heal(int amount) {
         currentHealth = std::min(maxHealth, currentHealth + amount);
+    }
+
+    void addItem(const Item &item) {
+        inventory.push_back(item);
+    }
+
+    void viewInventory() const {
+        if (inventory.empty()) {
+            std::cout << "Inventory is empty.\n";
+            return;
+        }
+
+        std::cout << "Inventory:\n";
+        for (size_t i = 0; i < inventory.size(); ++i) {
+            const Item& item = inventory[i];
+            std::cout << i << ": " << item.name
+                      << " - " << item.description
+                      << " [" << (item.isConsumable() ? "Consumable" : "Quest") << "]\n";
+        }
+    }
+
+    void useItem(int index) {
+        if (index < 0 || index >= inventory.size()) {
+            std::cout << "Invalid item index.\n";
+            return;
+        }
+
+        Item item = inventory[index];
+
+        if (item.isConsumable()) {
+            std::cout << "You used: " << item.name << "\n";
+            heal(item.healAmount);
+            inventory.erase(inventory.begin() + index); // Remove after use
+        } else {
+            std::cout << "This item cannot be used.\n";
+        }
     }
 };
 
